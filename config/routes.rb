e28d020d072698e326ea5b50c9c8d36a552954d3
kalunge <muthomititus+github@gmail.com>
2022-04-10 16:00:43 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api do
-    namespace :v1 do
-      resources :books
-    end
-  end
+  resources :books
+  get "/sorted", to: "books#sorted", as: :sorted
+  get "/sorted_by_author", to: "books#sorted_by_author", as: :author
+  get "/total_price", to: "books#total_price", as: :total
+  get "/save", to: "books#save_to_db", as: :save
 end
