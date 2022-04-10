79541bf670a014b78a7121f9c6cea3c543392794
kalunge <muthomititus+github@gmail.com>
2022-04-10 15:57:32 +0300

    Add api methods for sorting and adding many books from memory

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    render json: @books
+  end
+
+  def sorted
+    @sorted_books = Book.order('publisher, author_last_name, author_first_name, title')
+    render json: @sorted_books
+  end
+
+  def sorted_by_author
+    @sorted_by_author_books = Book.order('author_last_name, author_first_name, title')
+    render json: @sorted_by_author_books
+  end
+
+  def total_price
+    @total = Book.all.sum(:price)
+    render json: @total.round(2)
+  end
+
+  def save_to_db
+    @books_to_save = [{
+      publisher: 'Crossway', title: 'Eloquent Ruby', author_first_name: 'Russ', author_last_name: 'Olsen', price: 52.99
+    }, {
+      publisher: 'Ruby publishers', title: 'Eloquent Rails', author_first_name: 'Jane', author_last_name: 'Doe', price: 45.99
+    }, {
+      publisher: 'Js publishers', title: 'Eloquent Javascript', author_first_name: 'JOhn', author_last_name: 'Doe', price: 34.99
+    }, {
+      publisher: ' Clean code publishers', title: 'THe clean Coder ', author_first_name: 'Robert', author_last_name: 'Mcchain', price: 62.99
+    }]
+
+    @books_to_save.map { |book| Book.create!(book) }
+  end
+end
