8139df140efe92be1df79dab664618edb11af5fe
kalunge <muthomititus+github@gmail.com>
2022-04-10 15:59:30 +0300

    Add requests tests for api methods

--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -1,10 +1,9 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "BooksApi", type: :request do
-
-  describe "GET BOOKS" do
-    it "Returns all books" do
-      get "/api/v1/books"
+describe 'BooksApi', type: :request do
+  describe 'GET BOOKS' do
+    it 'Returns all books' do
+      get '/api/v1/books'
       expect(response).to have_http_status(:success)
     end
   end
